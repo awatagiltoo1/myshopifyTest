0dc230e9e78fb5f00e7aba453198881b253908fb
awatagiltoo1 <awagitagiltoo@gmail.com>
2024-10-30 15:15:33 +0000

    Ajout des POM

--- tests/InscriptionFixture.spec.ts
+++ tests/InscriptionFixture.spec.ts
@@ -5,7 +5,7 @@ import { Connexionpage } from '../pages/Connexionpage';
 
 
 
-test('Inscription utilisateur', async ({ inscriptionpage, page}) => {
+test('@KAN-3 Inscription utilisateur', async ({ inscriptionpage, page}) => {
   await inscriptionpage.goto();
   await inscriptionpage.inscrireUser('Awa THIOUNE', 'awatestauto@yopmail.com', 'Passer@123', 'Mamadou', 'DIOP', 'Agiltoo', 'Ouest Foire', 'SENEGAL',  'Dakar', '17893',
         '778888888',  '6', '6', '1998');
@@ -13,7 +13,7 @@ test('Inscription utilisateur', async ({ inscriptionpage, page}) => {
 });
 
 
-test('Connexion utilisateur', async ({ connexionpage, page}) => {
+test('@KAN-8 Connexion utilisateur', async ({ connexionpage, page}) => {
   await connexionpage.goto();
   await connexionpage.ConnexionUser('awatestauto@yopmail.com', 'Passer@123');
 
